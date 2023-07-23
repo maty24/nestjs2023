ed407a1c34dabe7571679f7fbaf754da646839e6
Matias Godoy <matiasgodoy@Matiass-Mac-mini.local>
2023-07-22 22:59:55 -0400

    base nestjs

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,65 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags: string[];
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  images?: string[];
+}
+
+export class PaginationDto {
+  @IsOptional()
+  @IsPositive()
+  @Type(() => Number) // enableImplicitConversions: true
+  limit?: number;
+
+  @IsOptional()
+  @Min(0)
+  @Type(() => Number) // enableImplicitConversions: true
+  offset?: number;
+}
